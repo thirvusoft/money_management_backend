468aaea51229a7d438744450b55a79f20c6831c2
Gokulc2002 <gokul@thirvusoft.in>
2022-03-23 20:48:11 +0530

    Daily entry reflect in chart of account

--- money_management_backend/money_management_backend/doctype/ts_daily_entry_sheet/ts_daily_entry_sheet.py
+++ money_management_backend/money_management_backend/doctype/ts_daily_entry_sheet/ts_daily_entry_sheet.py
@@ -1,8 +1,29 @@
-# Copyright (c) 2022, saheeth and contributors
+
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from datetime import date
 class TSDailyEntrySheet(Document):
-	pass
+	def after_insert(self):
+		account= frappe.get_value("TS Subtype", self.sub_type,"account")
+		doc=frappe.new_doc("Journal Entry")
+		acc=[{
+			"account": account,
+			"debit_in_account_currency":self.amount
+		},
+		{
+			"account": "Buildings - MM",
+			"credit_in_account_currency":self.amount
+		}
+		]
+		doc.update({
+			"company":"Money Manager",
+			"voucher_type":"Journal Entry",
+			"posting_date":date.today(),
+			"accounts": acc
+			})
+
+		doc.insert()
+		doc.submit()
+		frappe.db.commit()
