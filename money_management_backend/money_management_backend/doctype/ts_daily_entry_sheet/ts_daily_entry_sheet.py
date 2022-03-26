f47f18f6ed634250864030223cdffca3fb0a6186
Gokul C <95605286+Gokulc2002@users.noreply.github.com>
2022-03-26 11:43:09 +0530

    Update ts_daily_entry_sheet.py

--- money_management_backend/money_management_backend/doctype/ts_daily_entry_sheet/ts_daily_entry_sheet.py
+++ money_management_backend/money_management_backend/doctype/ts_daily_entry_sheet/ts_daily_entry_sheet.py
@@ -2,8 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.model.document 
-import Document
+from frappe.model.document import Document
 from datetime import date
 class TSDailyEntrySheet(Document):
 	def after_insert(self):
