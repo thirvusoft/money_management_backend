371ba29984cf3e487e5d013536b1b0b0ff82fa0e
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2022-04-04 12:55:43 +0530

    Revert "removed settings default"
    This reverts commit 1c7a3973e0bc4f81a4acf09be29194e7380f40ae.

--- money_management_backend/custom/py/defaults.py
+++ money_management_backend/custom/py/defaults.py
@@ -7,9 +7,9 @@ def createdocs():
        st.update({"doctype":"TS Subtype"})
        st.save()
        
-# setting_doc=frappe.new_doc('TS Settings')
-# setting_doc.update({
-#     'doctype': 'TS Settings',
-#     'account_to_credit': 'Buildings - MM'
-# })
-# setting_doc.save()
+setting_doc=frappe.new_doc('TS Settings')
+setting_doc.update({
+    'doctype': 'TS Settings',
+    'account_to_credit': 'Buildings - MM'
+})
+setting_doc.save()
